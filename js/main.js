1d0aa7863301d4c54f9a5120eaa0f8e9f513441d
Nikita A <shealtov@gmail.com>
2023-06-19 22:06:29 +0300

    Swiper added, need to be fixed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
-let button_close = document.getElementById("close-button");
-
 function closeAd() {
     document.querySelector(".banner__body").classList.add("banner__body--close");
 } 
+
+new Swiper('slider');
