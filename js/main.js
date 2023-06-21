27016e244b635c6a135d8b06c9797f00a381b332
Nikita A <shealtov@gmail.com>
2023-06-21 09:47:19 +0300

    Slider adaptivity added

--- js/main.js
+++ js/main.js
@@ -6,5 +6,22 @@ let swiper1 = new Swiper('.slider', {
     navigation: {
         nextEl: '.swiper-button-next',
         prevEl: '.swiper-button-prev'
-    }
-}); 
+    },
+
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false
+    },
+});
+
+let swiper2 = new Swiper('.slider2', {
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+    },
+
+    autoplay: {
+        delay: 7000,
+        disableOnInteraction: false
+    },
+});
