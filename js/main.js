744612f7f1c6856ddb7355f2e43c6e4b29f9feb2
Nikita A <shealtov@gmail.com>
2023-06-22 08:40:13 +0300

    Final stage, similar buttons/news to be fixed

--- js/main.js
+++ js/main.js
@@ -25,3 +25,28 @@ let swiper2 = new Swiper('.slider2', {
         disableOnInteraction: false
     },
 });
+
+function closeBurger() {
+    let burgerMenu = document.querySelector(".burger-menu");
+    burgerMenu.classList.remove("burger-menu--active");
+    burgerMenu.classList.add("burger-menu--close");
+}
+
+function openMenu() {
+    let burgerMenu = document.querySelector(".burger-menu");
+    burgerMenu.classList.remove("burger-menu--close");
+    burgerMenu.classList.add("burger-menu--active");
+}
+
+let date = new Date();
+let dateDay = date.getUTCDate();
+let dateMonth = date.getUTCMonth();
+let dateYear = date.getUTCFullYear();
+console.log();
+document.getElementById("fuel-price-date").textContent = dateDay + "." + (dateMonth + 1) + "." + dateYear;
+
+tablePrice = document.querySelectorAll(".fuel__type__element--date");
+
+for (let i = 0; i < tablePrice.length; i++) {
+    tablePrice[i].textContent = dateDay + "." + (dateMonth + 1) + "." + dateYear;
+}
